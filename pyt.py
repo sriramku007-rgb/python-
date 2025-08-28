77df56f03480cbd98b7abe3ff59f7aeba403550b
sriramku007-rgb <sriramku007@gmail.com>
2025-08-28 20:28:10 +0530

    Add files via upload

---
+++ pyt.py
@@ -0,0 +1,46 @@
+#  1 QUESTION Take two integers as input and print their sum, difference, and product.
+
+a=1
+b=2
+print("Sum:", a + b)
+print("Subtraction:", a - b)
+print("Product:", a*b)
+
+# 2 QUESTION Input a float and calculate the value after adding 18% tax.
+
+price = int(input("Enter the price before tax: " ))
+total_price = price *1.8
+print(f"Total price after 18% tax: {total_price:.2f}")
+
+# 3 question Reverse a string taken as input.
+
+text = input("enter a string: ")
+reversed_text = text[::-1]
+print(f"Reversed string: {reversed_text}")
+
+#4 question Given two boolean inputs, print their AND, OR, and NOT (first value).
+
+a = "true"
+b = "false"
+and_ok =a and b
+or_ok =a or b 
+not_ok = not b
+print (f"and : {and_ok}")
+print (f" or : {or_ok}")
+print (f"not: {not_ok}") 
+
+#5 questionIdentify and print the data type of user input after checking if it's int, float, or string.
+
+user_input = input("Enter something:")
+try:
+    # Try converting to integer
+    val = int(user_input)
+    print("Data type: int")
+except ValueError:
+    try:
+        # Try converting to float
+        val = float(user_input)
+        print("Data type: float")
+    except ValueError:
+        # If both fail, it's a string
+        print("Data type: string")
